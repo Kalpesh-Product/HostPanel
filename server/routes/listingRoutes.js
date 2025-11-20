6c6636f98bd7b5f95720e99992dba64596fe8340
muskan-wono <muskan.wono@gmail.com>
2025-11-20 10:58:52 +0530

    Added multer image config and added new route

--- server/routes/listingRoutes.js
+++ server/routes/listingRoutes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import upload from "../config/multerConfig.js";
+import upload, { uploadImages } from "../config/multerConfig.js";
 
 import {
+  activateProduct,
   createCompanyListing,
   editCompanyListing,
   getAllCompanyListings,
@@ -9,8 +10,9 @@ import {
 } from "../controllers/listingControllers.js";
 const router = Router();
 
-router.post("/add-company-listing", upload.any(), createCompanyListing);
-router.patch("/edit-company-listing", upload.any(), editCompanyListing);
+router.post("/add-company-listing", uploadImages.any(), createCompanyListing);
+router.patch("/edit-company-listing", uploadImages.any(), editCompanyListing);
+router.patch("/activate-product", activateProduct);
 router.get("/get-companies-listings", getAllCompanyListings);
 router.get("/get-company-listings", getCompanyListings);
 
