7ab1666a8af0221e911380a57652bc1941eb2588
muskan-wono <muskan.wono@gmail.com>
2026-02-10 18:27:11 +0530

    Send Panel name to populate the user Id correctly

---
+++ server/models/AdminUser.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const adminUserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+  },
+  refreshToken: {
+    type: String,
+  },
+});
+
+const AdminUser = mongoose.model("AdminUser", adminUserSchema);
+export default AdminUser;
